e72c7771c3c1029fc38b74f789b003a990268dca
matigonzalez232 <matias.gonzalez@estudiorochayasoc.com.ar>
2022-11-25 09:55:40 -0300

    google maps api javascript terminado

--- data.js
+++ data.js
@@ -1,8 +1,22 @@
-let hotels=[
+let hotels = [
     {
         name: "hotel almanac",
         lat: 41.3907533,
         lng: 2.1694134,
+        address: " gran via de les corts catalanes, 619-621",
+        phone: "93 111111",
+    },
+    {
+        name: "majestic hotel",
+        lat: 41.39348602294922,
+        lng: 2.1639552116394043,
+        address:" gran via de les corts catalanes, 619-621",
+        phone: "93 111111",
+    },
+    {
+        name: "miramar barcelona hotel",
+        lat: 41.37006759643555,
+        lng: 2.1714038848876953,
         address:" gran via de les corts catalanes, 619-621",
         phone: "93 111111",
     }
--- maps.js
+++ maps.js
@@ -16,10 +16,13 @@ const setListener = () => {
 const displayHotelList = () => {
     let hotelHTML = "";
     hotels.forEach(hotel => {
+        
         hotelHTML += `<h4 class="hotel__individualNames">${hotel.name}</h4>`
+        
+        // hotelHTML += `<h4 >${hotel.name}</h4>`
 
     })
-    document.getElementById("hotel__names").innetHTML = hotelHTML
+    document.getElementById("hotel__names").innerHTML = hotelHTML;
 }
 
 const createMarker = (coord, name, address, phone) => {
@@ -28,18 +31,18 @@ const createMarker = (coord, name, address, phone) => {
     <div class="window">
         <h2>${name}</h2>
         <div class="address">
-        <i class="fas fa-map-marker-alt fa-lg" ><i>
+        <i class="fas fa-map-marker-alt fa-lg" ></i>
         <h3>${address}</h3>
         </div>
-        <div class="address">
-        <i class="fas fa-phone-alt fa-lg" ><i>
+        <div class="phone">
+        <i class="fas fa-phone-alt fa-lg" ></i>
         <h3>${phone}</h3>
         </div>
     
     </div>
     
     `
-    const marker = new google.maps.marker(
+    const marker = new google.maps.Marker(
         {
             position: coord,
             map: map,
@@ -58,20 +61,22 @@ const createMarker = (coord, name, address, phone) => {
 const createLocationMarkers = () => {
     let bounds = new google.maps.LatLngBounds();
     hotels.forEach(hotel => {
-        let coord = new google.maLatLng(hotel.lat, hotel.lng);
+        let coord = new google.maps.LatLng(hotel.lat, hotel.lng);
         let name = hotel.name;
+        let address = hotel.address;
+        let phone = hotel.phone;
         bounds.extend(coord)
         createMarker(coord, name, address, phone);
         map.fitBounds(bounds);
     })
 }
 
-function initmap() {
+function initMap() {
     let barcelona = { lat: 41.390205, lng: 2.154007 }
     map = new google.maps.Map(document.getElementById("map"), {
         center: barcelona,
         zoom: 14,
-        // mapId: idDeEstilos
+        mapId: "8cce66f2ae190c68"
     })
 
     // console.log(hotels)
@@ -80,7 +85,7 @@ function initmap() {
         position: barcelona,
         map: map,
     })
-    infoWindow = new google.maps.infoWindow();
+    infoWindow = new google.maps.InfoWindow();
     displayHotelList();
     setListener();
 }
