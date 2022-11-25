3d0f50a3b4470e46bdc347af7a564848fa00c071
matigonzalez232 <matias.gonzalez@estudiorochayasoc.com.ar>
2022-11-25 12:12:58 -0300

    datos js 25/11

--- data.js
+++ data.js
@@ -1,43 +1,51 @@
-let hotels = [
-    {
-        name: "hotel almanac",
-        lat: 41.3907533,
-        lng: 2.1694134,
-        address: " gran via de les corts catalanes, 619-621",
-        phone: "93 111111",
-    },
-    {
-        name: "majestic hotel",
-        lat: 41.39348602294922,
-        lng: 2.1639552116394043,
-        address:" gran via de les corts catalanes, 619-621",
-        phone: "93 111111",
-    },
-    {
-        name: "miramar barcelona hotel",
-        lat: 41.37006759643555,
-        lng: 2.1714038848876953,
-        address:" gran via de les corts catalanes, 619-621",
-        phone: "93 111111",
-    }
-]
-let paradas= [
-    {
-        titulo: "parada 1",
-        lat: 41.3907533,
-        lng: 2.1694134,
-       subtitulo: "blalasldaksdksakdaskk"
-    },
-    {
-        titulo: "parada 2",
-        lat: 41.39348602294922,
-        lng: 2.1639552116394043,
-        subtitulo: "blalasldaksdksakdaskk"
-    },
-    {
-        titulo: "parada 3",
-        lat: 41.37006759643555,
-        lng: 2.1714038848876953,
-        subtitulo: "blalasldaksdksakdaskk"
-    }
-]
+let listado = {
+    hotels: [
+        {
+            name: "hotel almanac",
+            lat: 41.3907533,
+            lng: 2.1694134,
+            address: " gran via de les corts catalanes, 619-621",
+            phone: "93 111111",
+        },
+        {
+            name: "majestic hotel",
+            lat: 41.39348602294922,
+            lng: 2.1639552116394043,
+            address: " gran via de les corts catalanes, 619-621",
+            phone: "93 111111",
+        },
+        {
+            name: "miramar barcelona hotel",
+            lat: 41.37006759643555,
+            lng: 2.1714038848876953,
+            address: " gran via de les corts catalanes, 619-621",
+            phone: "93 111111",
+        },
+    ],
+    paradas: [
+        {
+            titulo: "parada 1",
+            lat: 41.3907533,
+            lng: 2.1694134,
+            subtitulo: "blalasldaksdksakdaskk"
+        },
+        {
+            titulo: "parada 2",
+            lat: 41.39348602294922,
+            lng: 2.1639552116394043,
+            subtitulo: "blalasldaksdksakdaskk"
+        },
+        {
+            titulo: "parada 3",
+            lat: 41.37006759643555,
+            lng: 2.1714038848876953,
+            subtitulo: "blalasldaksdksakdaskk"
+        }
+    ]
+}
+
+// listado.hotels.forEach((element) =>{
+//     console.log(element.name); 
+    
+// })
+// listado.hotels[0].name
